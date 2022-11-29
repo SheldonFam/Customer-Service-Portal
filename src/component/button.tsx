95bd9a646a5cd8302a5b3c2a0311fcd10816f695
Sheldon Fam <sheldonfam95@gmail.com>
2022-11-29 12:04:23 +0800

    remove node_modules

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -6,17 +6,20 @@ export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
   size?: "xs" | "sm" | "md" | "lg" | "xl";
   variant?: "solid" | "twoTone" | "plain" | "default";
   block?: boolean;
+  active?: boolean;
 }
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   function Button(
     {
       children,
-      size = "lg",
+      size = "md",
       shape = "round",
-      variant = "default",
+      variant = "solid",
       block,
       className,
+      active,
+      disabled,
       ...rest
     },
     ref
@@ -55,12 +58,91 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       return sizeClass;
     };
 
+    const disabledClass = "opacity-50 cursor-not-allowed";
+
+    const solidColor = () => {
+      const btn = {
+        bgColor: active ? `bg-green-700` : `bg-green-600`,
+        textColor: "text-white",
+        hoverColor: active ? "" : `hover:bg-green-500`,
+        activeColor: `active:bg-green-700`,
+      };
+      return getBtnColor(btn);
+    };
+
+    const twoToneColor = () => {
+      const btn = {
+        bgColor: active
+          ? `bg-green-200 dark:bg-green-50`
+          : `bg-green-50 dark:bg-green-500 dark:bg-opacity-20`,
+        textColor: `text-primary-600 dark:text-primary-50`,
+        hoverColor: active
+          ? ""
+          : `hover:bg-green-100 dark:hover:bg-green-500 dark:hover:bg-opacity-30`,
+        activeColor: `active:bg-green-200 dark:active:bg-green-500 dark:active:bg-opacity-40`,
+      };
+      return getBtnColor(btn);
+    };
+
+    const defaultColor = () => {
+      const btn = {
+        bgColor: active
+          ? `bg-gray-100 border border-gray-300 dark:bg-gray-500 dark:border-gray-500`
+          : `bg-white border border-gray-300 dark:bg-gray-700 dark:border-gray-700`,
+        textColor: `text-gray-600 dark:text-gray-100`,
+        hoverColor: active ? "" : `hover:bg-gray-50 dark:hover:bg-gray-600`,
+        activeColor: `active:bg-gray-100 dark:active:bg-gray-500 dark:active:border-gray-500`,
+      };
+      return getBtnColor(btn);
+    };
+
+    const plainColor = () => {
+      const btn = {
+        bgColor: active
+          ? `bg-gray-100 dark:bg-gray-500`
+          : "bg-transparent border border-transparent",
+        textColor: `text-gray-600 dark:text-gray-100`,
+        hoverColor: active ? "" : `hover:bg-gray-50 dark:hover:bg-gray-600`,
+        activeColor: `active:bg-gray-100 dark:active:bg-gray-500 dark:active:border-gray-500`,
+      };
+      return getBtnColor(btn);
+    };
+
+    const getBtnColor = ({
+      bgColor,
+      hoverColor,
+      activeColor,
+      textColor,
+    }: {
+      bgColor: string;
+      hoverColor: string;
+      activeColor: string;
+      textColor: string;
+    }) => {
+      return `${bgColor} ${
+        disabled ? disabledClass : hoverColor + " " + activeColor
+      } ${textColor}`;
+    };
+
+    const btnColor = () => {
+      switch (variant) {
+        case "solid":
+          return solidColor();
+        case "twoTone":
+          return twoToneColor();
+        case "plain":
+          return plainColor();
+        default:
+          return defaultColor();
+      }
+    };
     return (
       <button
         ref={ref}
         type="button"
         className={clsx(
-          "font-medium bg-green-600 text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2",
+          "font-semibold focus:outline-none focus-visible:ring focus-visible:ring-primary-200 whitespace-nowrap",
+          btnColor(),
           {
             round: "rounded-md",
             circle: "rounded-full",
@@ -70,6 +152,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           block && "w-full"
         )}
         {...rest}
+        disabled={disabled}
       >
         Button
       </button>
