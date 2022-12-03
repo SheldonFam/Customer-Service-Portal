ec6c9e2e83771c9168da07bffa3e59f2d7d53f31
Sheldon Fam <sheldonfam95@gmail.com>
2022-12-03 19:01:20 +0800

    pair programming and revise button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import { useState } from 'react';
-import reactLogo from './asset/react.svg';
-import './App.css';
-import { Button } from './component/button';
+import { Button } from "./component/button";
+import { useRef } from "react";
 
 function App() {
-  return (
-    <div>
-      <Button />
+  const buttonref = useRef<HTMLButtonElement>(null);
+  console.log(buttonref);
+  const result = (
+    <div className="flex items-center justify-center my-2">
+      <Button size="xs" variant="primary" shape="none">
+        primary
+      </Button>
+      <Button
+        size="sm"
+        variant="secondary"
+        shape="round"
+        onClick={() => {
+          console.log(buttonref);
+          buttonref.current?.focus();
+        }}
+      >
+        secondary
+      </Button>
+      <Button size="md" variant="white" shape="circle">
+        white
+      </Button>
+      <Button size="lg" variant="default" shape="round">
+        default
+      </Button>
+      <Button size="xl" variant="primary" ref={buttonref}>
+        button
+      </Button>
     </div>
   );
+  console.log(result);
+  return result;
 }
 
 export default App;
--- src/component/button.tsx
+++ src/component/button.tsx
@@ -4,86 +4,46 @@ import * as React from "react";
 export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
   shape?: "round" | "circle" | "none";
   size?: "xs" | "sm" | "md" | "lg" | "xl";
-  variant?: "solid" | "twoTone" | "plain" | "default";
+  variant?: "primary" | "secondary" | "white" | "default";
   children?: React.ReactNode;
 }
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   function Button(
-    {
-      children = "Button",
-      size = "md",
-      shape = "round",
-      variant = "solid",
-      className,
-      disabled,
-      ...rest
-    },
-    ref
+    { children, size = "md", shape, variant = "default", ...rest },
+    forwardedRef
   ) {
-    const buttonSize = size;
-
-    const getButtonSize = () => {
-      let sizeClass = "";
-      switch (buttonSize) {
-        case "xl":
-          sizeClass = clsx(
-            "h-16 items-center justify-center px-10 py-2 text-xl"
-          );
-          break;
-        case "lg":
-          sizeClass = clsx(
-            "h-14 items-center justify-center px-8 py-2 text-lg"
-          );
-          break;
-        case "sm":
-          sizeClass = clsx(
-            "h-9  items-center justify-center  px-3 py-2 text-sm"
-          );
-          break;
-        case "xs":
-          sizeClass = clsx(
-            "h-7  items-center justify-center px-3 py-1 text-xs"
-          );
-          break;
-        default:
-          sizeClass = clsx(
-            "h-11 items-center justify-center text-base px-8 py-2"
-          );
-          break;
-      }
-      return sizeClass;
-    };
-
-    const btnColor = () => {
-      switch (variant) {
-        case "solid":
-          return "bg-green-700 text-white hover:bg-green-500 active:bg-green-700";
-        case "twoTone":
-          return "bg-green-200 text-green-700 hover:bg-green-100 active:bg-green-200";
-        case "plain":
-          return "bg-white text-gray-700 hover:bg-gray-50 active:bg-gray-100";
-        default:
-          return "bg-gray-100 text-gray-600 hover:bg-gray-50 active:bg-gray-100";
-      }
-    };
-
     return (
       <button
-        ref={ref}
+        ref={forwardedRef}
         type="button"
         className={clsx(
-          "font-semibold focus:outline-none focus-visible:ring focus-visible:ring-primary-200 whitespace-nowrap",
-          btnColor(),
+          "mx-4 font-medium focus:outline-none flex items-center justify-center",
           {
-            round: "rounded-md",
-            circle: "rounded-full",
-            none: "rounded-none",
-          }[shape],
-          getButtonSize()
+            xl: " px-6 py-3 text-lg",
+            lg: " px-4 py-2 text-lg",
+            sm: " px-3 py-2 text-sm",
+            xs: " px-2.5 py-1.5 text-xs",
+            md: " text-base px-4 py-2",
+          }[size],
+          {
+            primary:
+              "bg-green-600 text-white hover:bg-green-700 active:bg-green-700",
+            secondary:
+              "bg-green-100 text-green-700 hover:bg-green-200 active:bg-green-200",
+            white: "bg-white text-gray-700 hover:bg-gray-50 active:bg-gray-100",
+            default:
+              "bg-gray-100 text-gray-600 hover:bg-gray-50 active:bg-gray-100",
+          }[variant],
+
+          shape &&
+            {
+              round: "rounded-md",
+              circle: "rounded-full",
+              none: "rounded-none",
+            }[shape]
         )}
         {...rest}
-        disabled={disabled}
       >
         {children}
       </button>
