510c68a4f0f618be7744df23384202abaf92c5c2
Sheldon Fam <sheldonfam95@gmail.com>
2022-12-05 21:11:53 +0800

    setup storybook

---
+++ src/component/Button.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Button } from "./button";
+
+export default {
+  title: "Example/Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (props) => (
+  <Button {...props}>Button</Button>
+);
+
+export const SolidButton = Template.bind({});
+SolidButton.args = {
+  variant: "primary",
+};
