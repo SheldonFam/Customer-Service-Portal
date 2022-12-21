e88ae15ef433fd6497a7771881fdc89869f3ab7a
Sheldon Fam <sheldonfam95@gmail.com>
2022-12-21 22:58:03 +0800

    add props in dialog component

--- src/component/dialog.stories.tsx
+++ src/component/dialog.stories.tsx
@@ -1,15 +1,41 @@
-import React from "react";
+// import React from "react";
+// import { ComponentStory, ComponentMeta } from "@storybook/react";
+// import { DialogDemo } from "./dialog";
+
+// export default {
+//   title: "Dialog",
+//   component: DialogDemo,
+// } as ComponentMeta<typeof DialogDemo>;
+
+// const Template: ComponentStory<typeof DialogDemo> = () => (
+//   <DialogDemo></DialogDemo>
+// );
+
+// export const Default = Template.bind({});
+// Default.args = {};
+
+import React, { useState } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { DialogDemo } from "./dialog";
+import { Dialog } from "./dialog";
+import { Button } from "./button";
 
 export default {
   title: "Dialog",
-  component: DialogDemo,
-} as ComponentMeta<typeof DialogDemo>;
+  component: Dialog,
+} as ComponentMeta<typeof Dialog>;
 
-const Template: ComponentStory<typeof DialogDemo> = () => (
-  <DialogDemo></DialogDemo>
-);
+const Template: ComponentStory<typeof Dialog> = (props) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>Open</Button>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <p>Content</p>
+        <button onClick={() => setOpen(false)}>Close</button>
+      </Dialog>
+    </>
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {};
