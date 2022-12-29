46e6b9ec0b4b972480d2d80e4350cdb249ba82c6
Sheldon Fam <sheldonfam95@gmail.com>
2022-12-29 18:26:54 +0800

    add button in report pages

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -23,10 +23,26 @@
 //     </div>
 //   );
 // };
+import { Button } from "../component/button";
+import { useNavigate } from "react-router-dom";
 
 export const ReportPages = () => {
+  const navigate = useNavigate();
+
+  const returnToHome = () => {
+    navigate("/");
+  };
   return (
     <div className="mx-auto max-w-7xl px-4 pt-8 sm:px-6 lg:px-8">
+      <div className="flex flex-row justify-between mb-2">
+        <div>
+          <Button onClick={returnToHome}>Go Back</Button>
+        </div>
+        <div className="flex flex-row gap-2">
+          <Button variant={"primary"}>Edit</Button>
+          <Button variant={"danger"}>Delete</Button>
+        </div>
+      </div>
       <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
         <div className="px-4 py-5 sm:px-6">
           <div>Report</div>
